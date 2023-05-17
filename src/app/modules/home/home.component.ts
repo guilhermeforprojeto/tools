a5f78cd3401a060375b7731fbe30e3a030379eba
guilherme <guilherme@conceitho.com>
2023-05-17 14:35:19 -0300

    feat: Add dados via api

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { PoMenuItem } from '@po-ui/ng-components';
 
 @Component({
@@ -6,13 +7,12 @@ import { PoMenuItem } from '@po-ui/ng-components';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
-
+export class HomeComponent implements OnInit {
   readonly menus: Array<PoMenuItem> = [
     { label: 'Home', action: this.onClick.bind(this) }
   ];
 
-  filtersOptions  = [
+  filtersOptions = [
     {
       property: 'nome'
     },
@@ -26,124 +26,31 @@ export class HomeComponent {
       property: 'ano',
       type: 'date'
     }
-  ]
+  ];
 
-  tools: Array<any> = [
-    {
-      "nome": "Carburador",
-      "marca": "Uno",
-      "ano": "2019",
-      "categoria": "Moto",
-      "valor": "29.00"
-    },
-    {
-      "nome": "Pneu",
-      "marca": "Michelin",
-      "ano": "N/A",
-      "categoria": "Carro",
-      "valor": "150.00"
-    },
-    {
-      "nome": "Bateria",
-      "marca": "Moura",
-      "ano": "N/A",
-      "categoria": "Carro",
-      "valor": "200.00"
-    },
-    {
-      "nome": "Amortecedor",
-      "marca": "Cofap",
-      "ano": "N/A",
-      "categoria": "Carro",
-      "valor": "120.00"
-    },
-    {
-      "nome": "Vela de Ignição",
-      "marca": "NGK",
-      "ano": "N/A",
-      "categoria": "Carro",
-      "valor": "15.00"
-    },
-    {
-      "nome": "Óleo de Motor",
-      "marca": "Mobil",
-      "ano": "N/A",
-      "categoria": "Carro",
-      "valor": "40.00"
-    },
-    {
-      "nome": "Pastilha de Freio",
-      "marca": "Bendix",
-      "ano": "N/A",
-      "categoria": "Carro",
-      "valor": "50.00"
-    },
-    {
-      "nome": "Correia Dentada",
-      "marca": "Contitech",
-      "ano": "N/A",
-      "categoria": "Carro",
-      "valor": "80.00"
-    },
-    {
-      "nome": "Filtro de Ar",
-      "marca": "Tecfil",
-      "ano": "N/A",
-      "categoria": "Carro",
-      "valor": "20.00"
-    },
-    {
-      "nome": "Radiador",
-      "marca": "Valeo",
-      "ano": "N/A",
-      "categoria": "Carro",
-      "valor": "250.00"
-    },
-    {
-      "nome": "Lâmpada",
-      "marca": "Philips",
-      "ano": "N/A",
-      "categoria": "Carro",
-      "valor": "10.00"
-    },
-    {
-      "nome": "Embreagem",
-      "marca": "Luk",
-      "ano": "N/A",
-      "categoria": "Carro",
-      "valor": "300.00"
-    },
-    {
-      "nome": "Cabo de Vela",
-      "marca": "NGK",
-      "ano": "N/A",
-      "categoria": "Carro",
-      "valor": "25.00"
-    },
-    {
-      "nome": "Sensor de Velocidade",
-      "marca": "Bosch",
-      "ano": "N/A",
-      "categoria": "Carro",
-      "valor": "60.00"
-    },
-    {
-      "nome": "Termostato",
-      "marca": "Vernet",
-      "ano": "N/A",
-      "categoria": "Carro",
-      "valor": "30.00"
-    },
-    {
-      "nome": "Bomba de Combustível",
-      "marca": "Bosch",
-      "ano": "N/A",
-      "categoria":"",
-      "valor":"30:00"
-    }]
+  tools: Array<any> = [];
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+    this.getToolsData();
+  }
 
   private onClick() {
-    alert('Clicked in menu item')
+    alert('Clicked in menu item');
   }
+
+  private getToolsData() {
+    this.http.get<any>('https://my-json-server.typicode.com/guilhermeforprojeto/order_your_parts-api/orderparts').subscribe(
+      (response) => {
+        this.tools = response.parts ?? [];
+      },
+      (error) => {
+        console.error("ERROR");
+        console.error('Failed to fetch tools data:', error);
+      }
+    );
+  }
+
 
 }
